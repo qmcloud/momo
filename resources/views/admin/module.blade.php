602a33cf9c141d200eca44af0ba93ce433295bb3
json <111@qq.com>
2020-08-27 12:59:21 +0800

    remove

--- resources/views/admin/module.blade.php
+++
@@ -1,3 +0,0 @@
-<iframe name="iframeMain"  src="{{route('module-list',['id' => $specialId])}}"  frameborder="0" width="100%" height="100%" style="min-height: 740px">
-    该浏览器不支持iframe，请使用其他浏览器！
-</iframe>
--- resources/views/admin/tools/check_goods.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="btn-group" >
-    <button type="button" class="btn btn-block btn-default disabled" id="submitCheckoutGoods">添加已选</button>
-</div>
